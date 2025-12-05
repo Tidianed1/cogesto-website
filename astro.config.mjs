18f8b6daefd06db870602a2cd31392735818ca8e
Tidianed1 <tidianed94@gmail.com>
2025-12-05 01:24:13 +0100

    fix: Update CSS paths and add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: 'static',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
